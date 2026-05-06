38ce9d051293fe32e47143ded5521aa62c914c16
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-05-06 19:19:53 +0700

    Update xsd_validator.py

--- .github/scripts/xsd_validator.py
+++ .github/scripts/xsd_validator.py
@@ -1,6 +1,10 @@
 from lxml import etree
 import sys
 def main():
+ if len(sys.argv)<2:
+  print("✨ usage")
+  print(f"🪜 {sys.argv[0]} <schema.xsd>")
+  return 1
  return 0
 if __name__=="__main__":
  sys.exit(main())
