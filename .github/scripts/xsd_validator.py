8102abaf15776471602219354ae1171bd12f6dcc
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-05-06 19:24:33 +0700

    Update xsd_validator.py

--- .github/scripts/xsd_validator.py
+++ .github/scripts/xsd_validator.py
@@ -2,13 +2,37 @@ from lxml import etree
 import sys
 class Validator:
  def __init__(self):
- def validate_xsd(self,path):
-  
+  self.tmp_xsd=None
+ def load_xsd(self,schema_source:str):
+  try:
+   if schema_source.startswith("http://") or schema_source.startswith("https://"):
+    with urllib.request.urlopen(schema_source) as r:
+     data=r.read()
+    self.tmp_xsd=tempfile.NamedTemporaryFile(suffix=".xsd")
+    self.tmp_xsd.write(data)
+    self.tmp_xsd.flush()
+    schema_path=self.tmp_xsd.name
+   else:
+    schema_path=schema_source
+   tree=etree.parse(schema_path)
+   etree.XMLSchema(tree)
+   return True
+  except Exception as e:
+   print(e)
+   return False
+ def cleanup_cache(self):
+  if self.tmp_xsd:
+   if not self.tmp_xsd is str:
+    self.tmp_xsd.close()
+  if self.tmp_target:
+   if not self.tmp_target is str:
+    self.tmp_target.close()
 def main():
  if len(sys.argv)<2:
   print("✨ usage")
   print(f"🪜 {sys.argv[0]} <schema.xsd>")
   return 1
+ validator=Validator()
  return 0
 if __name__=="__main__":
  sys.exit(main())
